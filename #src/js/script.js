dc73fc6779c436038abe8d118b3782199459cd2a
JusticeUA <vasya.ganec@gmail.com>
2021-09-19 22:28:26 +0300

    header done

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,2 +1,26 @@
-@@include('testWebP.js')
+@@include('files/functions.js', {});
 
+cl = document.querySelectorAll('.menu__item')
+function _removeClasses() {
+    
+    for (var i = 0; i < cl.length; i++) {
+      cl[i].classList.remove("_hover")
+    }
+   }
+
+window.onload = function () {
+    document.addEventListener("click", documentActions);
+
+    //Actions (делегування події click)
+    function documentActions(e) {
+        const targetElement = e.target;
+        if(window.innerWidth>768 && isMobile.any()){
+            if(targetElement.classList.contains('menu__arrow')){
+                targetElement.closest('.menu__item').classList.toggle('_hover')
+            }
+            if(!targetElement.closest('.menu__item') ){
+                _removeClasses()
+            }
+        }
+    }
+}
